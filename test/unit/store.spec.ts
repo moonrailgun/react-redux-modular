eb74aad18194382468a07e2e41d519304314ee73
moonrailgun <moonrailgun@gmail.com>
2019-12-23 15:08:48 +0800

    remove unuse testcase

--- test/unit/store.spec.ts
+++ test/unit/store.spec.ts
@@ -1,36 +1,7 @@
-import { createReducer, createStore } from '../../src/store';
+import { createStore } from '../../src/store';
 import { UserModel } from '../models/user';
 
 describe('store', () => {
-  describe('createReducer', () => {
-    test('createReducer should be ok', () => {
-      const reducer = createReducer('user', new UserModel());
-
-      expect(reducer).toBeTruthy();
-      expect(typeof reducer).toBe('function');
-    });
-
-    test('reducer should be return origin state if not match any type', () => {
-      const reducer = createReducer('user', new UserModel());
-
-      const prevState = {
-        username: 'test',
-      };
-      const nextState = reducer(prevState, { type: 'any' });
-      expect(nextState).toBe(prevState);
-    });
-
-    test('reducer shoule be return new state if match any type', () => {
-      const reducer = createReducer('user', new UserModel());
-
-      const prevState = {
-        username: 'test',
-      };
-      const nextState = reducer(prevState, { type: 'user/any' });
-      expect(nextState).not.toBe(prevState);
-    });
-  });
-
   describe('createStore', () => {
     test('createStore should be ok', () => {
       const store = createStore({
